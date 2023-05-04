48f49214bc3d304d7b9afcaa54f1ff9095c26b24
Kristoffer Dalby <kristoffer@tailscale.com>
2023-05-04 15:21:50 +0200

    dap: tailcontrol2
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- lua/debuuug.lua
+++ lua/debuuug.lua
@@ -10,7 +10,7 @@ require("dap-go").setup({
             type =  "go",
             request =  "launch",
             mode =  "exec",
-            program = vim.fn.getcwd() .. "/cmd/tailcontrol/main.go",
+            program = vim.fn.getcwd() .. "/cmd/tailcontrol/",
             args =  {
                 "-dev",
                 "-confdir=./ignore",
@@ -30,15 +30,14 @@ require("dap-go").setup({
     })
 require("nvim-dap-virtual-text").setup()
 
-
-vim.keymap.set("n", "<leader>dc", dap.continue, {silent = true, noremap = true})
-vim.keymap.set("n", "<leader>ds", dap.step_over, {silent = true, noremap = true})
-vim.keymap.set("n", "<leader>dsi", dap.step_into, {silent = true, noremap = true})
-vim.keymap.set("n", "<leader>dso", dap.step_out, {silent = true, noremap = true})
-vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {silent = true, noremap = true})
-vim.keymap.set("n", "<leader>dui", dapui.toggle, {silent = true, noremap = true})
-vim.keymap.set("n", "<leader>dro", dap.repl.open, {silent = true, noremap = true})
-vim.keymap.set("n", "<leader>dcc", tse.dap.commands, {silent = true, noremap = true})
-vim.keymap.set("n", "<leader>dlb", tse.dap.list_breakpoints, {silent = true, noremap = true})
-vim.keymap.set("n", "<leader>dv", tse.dap.variables, {silent = true, noremap = true})
-vim.keymap.set("n", "<leader>df", tse.dap.frames, {silent = true, noremap = true})
+vim.keymap.set("n", "<leader>dc", dap.continue, {})
+vim.keymap.set("n", "<leader>ds", dap.step_over, {})
+vim.keymap.set("n", "<leader>dsi", dap.step_into, {})
+vim.keymap.set("n", "<leader>dso", dap.step_out, {})
+vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
+vim.keymap.set("n", "<leader>dui", dapui.toggle, {})
+vim.keymap.set("n", "<leader>dro", dap.repl.open, {})
+vim.keymap.set("n", "<leader>dcc", tse.dap.commands, {})
+vim.keymap.set("n", "<leader>dlb", tse.dap.list_breakpoints, {})
+vim.keymap.set("n", "<leader>dv", tse.dap.variables, {})
+vim.keymap.set("n", "<leader>df", tse.dap.frames, {})
