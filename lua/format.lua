af7d03a7ea70653f9d2d519ef06fed6c4c46b2de
Kristoffer Dalby <kristoffer@dalby.cc>
2026-03-02 14:12:35 +0000

    lsp: replace none-ls with conform.nvim and nvim-lint
    none-ls (null-ls) had 9+ broken builtins and is intentionally shrinking
    scope, recommending dedicated tools instead. Replace with:
    
    - conform.nvim for formatting (format-on-save, `<leader>ff`)
    - nvim-lint for linting (on BufWritePost, BufReadPost, InsertLeave)
    
    All formatters and linters from the previous config are preserved.
    The Go goimports conditional (only in repos with go.toolchain.rev)
    is reimplemented using conform's function-based formatters_by_ft.
    
    lsp.lua is reduced to pure vim.lsp.config() and vim.lsp.enable() calls
    now that lspsaga, null-ls, and cmp_nvim_lsp have all been removed.

---
+++ lua/format.lua
@@ -0,0 +1,102 @@
+require("conform").setup({
+  formatters_by_ft = {
+    -- Nix
+    nix = { "alejandra" },
+
+    -- Python: isort first (sorts imports), then black (formats code)
+    python = { "isort", "black" },
+
+    -- Go: conditional goimports + golines
+    go = function(bufnr)
+      local formatters = {}
+      -- Only use goimports in repos that have a go.toolchain.rev file
+      local go_toolchain = vim.fs.find("go.toolchain.rev", {
+        upward = true,
+        path = vim.fs.dirname(vim.api.nvim_buf_get_name(bufnr)),
+      })
+      if #go_toolchain > 0 then
+        table.insert(formatters, "goimports")
+      end
+      table.insert(formatters, "golines")
+      return formatters
+    end,
+
+    -- Protobuf
+    proto = { "buf" },
+
+    -- Markdown: cbfmt for code blocks
+    markdown = { "cbfmt" },
+
+    -- C/C++
+    c = { "clang-format" },
+    cpp = { "clang-format" },
+
+    -- Django/Jinja templates
+    htmldjango = { "djlint" },
+
+    -- Fish shell
+    fish = { "fish_indent" },
+
+    -- JSON
+    json = { "jq" },
+    jsonc = { "jq" },
+
+    -- JS/TS/HTML/CSS: prettierd with prettier as fallback
+    javascript = { "prettierd", "prettier", stop_after_first = true },
+    javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+    typescript = { "prettierd", "prettier", stop_after_first = true },
+    typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+    html = { "prettierd", "prettier", stop_after_first = true },
+    css = { "prettierd", "prettier", stop_after_first = true },
+    scss = { "prettierd", "prettier", stop_after_first = true },
+    less = { "prettierd", "prettier", stop_after_first = true },
+    vue = { "prettierd", "prettier", stop_after_first = true },
+    graphql = { "prettierd", "prettier", stop_after_first = true },
+
+    -- Shell: shellharden first (fixes quoting), then shfmt (formats)
+    sh = { "shellharden", "shfmt" },
+    bash = { "shellharden", "shfmt" },
+
+    -- Swift
+    swift = { "swiftformat" },
+
+    -- HCL (Packer/Terraform)
+    hcl = { "packer_fmt" },
+    packer = { "packer_fmt" },
+
+    -- XML via tidy
+    xml = { "tidy" },
+
+    -- CSS linting/formatting via stylelint
+    -- (prettier handles CSS by default above, uncomment if you prefer stylelint)
+    -- css = { "stylelint" },
+    -- scss = { "stylelint" },
+  },
+
+  -- Format on save
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
+
+  -- Custom formatter definitions for tools not built into conform
+  formatters = {
+    tidy = {
+      command = "tidy",
+      args = {
+        "-quiet",
+        "-xml",
+        "--indent", "auto",
+        "--indent-spaces", "2",
+        "--wrap", "80",
+        "--sort-attributes", "alpha",
+        "--tidy-mark", "no",
+      },
+      stdin = true,
+      exit_codes = { 0, 1 },
+    },
+  },
+})
+
+-- Set formatexpr for gq operator
+vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,90 +1,4 @@
-local null_ls = require("null-ls")
-local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
-
-null_ls.setup(
-  {
-    capabilities = capabilities,
-    on_attach = function(client, bufnr)
-      if client:supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = augroup,
-          buffer = bufnr,
-          callback = function()
-            vim.lsp.buf.format({ bufnr = bufnr })
-          end,
-        })
-      end
-    end,
-    sources = {
-      null_ls.builtins.code_actions.eslint_d,
-      null_ls.builtins.code_actions.shellcheck,
-      null_ls.builtins.code_actions.statix,
-      null_ls.builtins.completion.spell,
-      null_ls.builtins.diagnostics.editorconfig_checker.with(
-        {
-          args = { "-disable-indentation", "-no-color", "$FILENAME" },
-        }
-      ),
-      null_ls.builtins.diagnostics.actionlint,
-      null_ls.builtins.diagnostics.buf,
-      null_ls.builtins.diagnostics.commitlint,
-      null_ls.builtins.diagnostics.curlylint,
-      null_ls.builtins.diagnostics.deadnix,
-      null_ls.builtins.diagnostics.djlint,
-      null_ls.builtins.diagnostics.eslint_d,
-      null_ls.builtins.diagnostics.fish,
-      null_ls.builtins.diagnostics.flake8,
-      null_ls.builtins.diagnostics.gitlint,
-      null_ls.builtins.diagnostics.hadolint,
-      null_ls.builtins.diagnostics.mypy,
-      null_ls.builtins.diagnostics.pylama,
-      null_ls.builtins.diagnostics.shellcheck,
-      null_ls.builtins.diagnostics.staticcheck,
-      null_ls.builtins.diagnostics.statix,
-      null_ls.builtins.diagnostics.stylelint,
-      null_ls.builtins.diagnostics.tidy,
-      null_ls.builtins.diagnostics.vale,
-      null_ls.builtins.diagnostics.write_good,
-      null_ls.builtins.diagnostics.yamllint,
-      null_ls.builtins.formatting.alejandra,
-      null_ls.builtins.formatting.beautysh,
-      null_ls.builtins.formatting.black,
-      null_ls.builtins.formatting.buf,
-      null_ls.builtins.formatting.cbfmt,
-      null_ls.builtins.formatting.clang_format,
-      null_ls.builtins.formatting.djlint,
-      null_ls.builtins.formatting.eslint_d,
-      null_ls.builtins.formatting.fish_indent,
-      null_ls.builtins.formatting.goimports.with({
-        condition = function(utils)
-          -- Try to detect if we are in a tailscale repo
-          return utils.root_has_file({ "go.toolchain.rev" })
-        end,
-      }),
-      null_ls.builtins.formatting.golines.with({
-        condition = function(utils)
-          return not utils.root_has_file({ "go.toolchain.rev" })
-        end,
-      }),
-      null_ls.builtins.formatting.isort,
-      null_ls.builtins.formatting.jq,
-      null_ls.builtins.formatting.tidy,
-      null_ls.builtins.formatting.prettier_d_slim,
-      null_ls.builtins.formatting.shellharden,
-      null_ls.builtins.formatting.shfmt,
-      null_ls.builtins.formatting.stylelint,
-      null_ls.builtins.formatting.swiftformat,
-      -- null_ls.builtins.formatting.terraform_fmt, -- Covered by LSP?
-      null_ls.builtins.formatting.trim_newlines,
-      null_ls.builtins.formatting.packer,
-      null_ls.builtins.hover.dictionary
-    }
-  }
-)
-
 vim.lsp.config('jsonls', {
-  capabilities = capabilities,
   settings = {
     json = {
       schemas = require("schemastore").json.schemas(),
@@ -93,7 +7,6 @@ vim.lsp.config('jsonls', {
 })
 
 vim.lsp.config('lua_ls', {
-  capabilities = capabilities,
   settings = {
     Lua = {
       format = {
@@ -114,8 +27,8 @@ vim.lsp.config('lua_ls', {
     },
   },
 })
+
 vim.lsp.config('gopls', {
-  capabilities = capabilities,
   filetypes = { "go", "gomod", "gotexttmpl", "gohtmltmpl" },
   settings = {
     gopls = {
@@ -127,7 +40,6 @@ vim.lsp.config('gopls', {
 })
 
 vim.lsp.config('yamlls', {
-  capabilities = capabilities,
   filetypes = {
     "yaml", "yaml.ansible", "ansible"
   },
@@ -142,62 +54,12 @@ vim.lsp.config('yamlls', {
 })
 
 vim.lsp.config('ansiblels', {
-  capabilities = capabilities,
   filetypes = { "yaml", "yaml.ansible", "ansible" },
   root_markers = { "requirements.yaml", "inventory" },
 })
 
-vim.lsp.config('nil_ls', min)
-vim.lsp.config('nixd', {
-  -- Disable formatting as we want to use Alejandra from
-  -- null-ls instead.
-  capabilities = capabilities,
-})
-vim.lsp.config('elmls', min)
-vim.lsp.config('terraformls', min)
-vim.lsp.config('buf_ls', min)
-vim.lsp.config('dhall_lsp_server', min)
-vim.lsp.config('golangci_lint_ls', min)
-vim.lsp.config('sourcekit', min)
-vim.lsp.config('ts_ls', min)
-vim.lsp.config('pyright', min)
-vim.lsp.config('cssls', {
-  capabilities = capSnippets,
-})
-vim.lsp.config('html', {
-  capabilities = capSnippets,
-})
-vim.lsp.config('tailwindcss', {
-  -- Disable formatting as we want to use Alejandra from
-  -- null-ls instead.
-  capabilities = capabilities,
-  filetypes = {
-    "go"
-  },
-  settings = {
-    tailwindCSS = {
-      lint = {
-        cssConflict = "warning",
-        invalidApply = "error",
-        invalidConfigPath = "error",
-        invalidScreen = "error",
-        invalidTailwindDirective = "error",
-        recommendedVariantOrder = "warning",
-        unusedClass = "warning",
-      },
-      experimental = {
-        classRegex = {
-          'a.Class: "([^"]*)"',
-        },
-      },
-      validate = true,
-    },
-  },
-})
+vim.lsp.config('nixd', {})
 
-vim.lsp.config('rust_analyzer', min)
-
--- Enable all configured LSP servers
 vim.lsp.enable({
   'jsonls',
   'lua_ls',
