6575f3cb1c4c8f7739f79cfd16f976d49dc92049
Kristoffer Dalby <kristoffer@dalby.cc>
2026-03-02 14:04:36 +0000

    all: remove dead plugins, update stale URLs, fix config bugs
    Remove plugins that are unused, archived, or superseded by builtins:
    - nvim-ts-rainbow: archived, incompatible with current treesitter
    - lsp-status.nvim: never required in config, superseded by vim.lsp.status()
    - lspkind-nvim: installed but completely commented out
    - Comment.nvim: superseded by Neovim 0.10+ builtin gc/gcc
    - vim-polyglot: conflicts with treesitter, unmaintained since 2020
    - vim-cooklang: treesitter has cooklang grammar
    - octo.nvim: installed but setup() never called
    - telescope-ui-select.nvim: installed but extension never loaded
    - cwebster2/github-coauthors.nvim: redundant with co-author.nvim
    
    Update stale plugin source URLs:
    - nvim-colorizer.lua: norcalli (abandoned) -> catgoose (active fork)
    - sqlite.lua: tami5 -> kkharji (same author, renamed account)
    - nvim-web-devicons: kyazdani42 -> nvim-tree (renamed org)
    
    Fix config bugs:
    - foldexpr: use native vim.treesitter.foldexpr() instead of removed
      nvim-treesitter plugin API
    - tokyonight: migrate from removed vim.g config vars to setup() API
    - Remove dead TroubleToggle/TodoTrouble keybindings (trouble.nvim is
      not installed)

--- init.lua
+++ init.lua
@@ -4,8 +4,6 @@ require("statusline")
 require("completion")
 require("todo")
 require("lsp")
-require("co-author")
-
 local cmd = vim.cmd -- to execute Vim commands e.g. cmd('pwd')
 local fn = vim.fn   -- to call Vim functions e.g. fn.bufnr()
 local g = vim.g     -- a table to access global variables))
@@ -15,22 +13,22 @@ local keymap = vim.keymap
 g.mapleader = " "
 
 opt.foldmethod = "expr"
-opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 opt.foldlevel = 20
 
 local indent = 4
+require("tokyonight").setup({
+  style = "night",
+  transparent = true,
+})
 cmd "silent! colorscheme tokyonight"
 cmd "set signcolumn=yes"
-
-g.tokyonight_style = "night"
-g.tokyonight_transparent = true
 
 opt.expandtab = true    -- Use spaces instead of tabs
 opt.shiftwidth = indent -- Size of an indent
 opt.smartindent = true  -- Insert indents automatically
 opt.tabstop = indent    -- Number of spaces tabs count for
 opt.completeopt = { "menu", "menuone", "noinsert", "noselect" }
--- opt.completeopt = {"menuone", "noinsert", "noselect"}
 opt.hidden = true                                                          -- Enable modified buffers in background
 opt.ignorecase = true                                                      -- Ignore case
 opt.joinspaces = false                                                     -- No double spaces with join after a dot
@@ -53,9 +51,6 @@ keymap.set("n", "<leader>o", "m`o<Esc>``", {}) -- Insert a newline in normal mod
 
 keymap.set('n', '<leader>ff', vim.lsp.buf.format, {})
 
-keymap.set("n", "<leader>tt", "<cmd>:TroubleToggle<cr>", {}) -- Toggle trouble
-keymap.set("n", "<leader>to", "<cmd>:TodoTrouble<cr>", {})   -- Toggle trouble
-
 keymap.set("n", "<leader>h", "<cmd>:Lspsaga hover_doc<cr>", {})
 keymap.set("n", "<leader>r", "<cmd>:Lspsaga rename<cr>", {})
 keymap.set("n", "<leader>a", "<cmd>:Lspsaga code_action<cr>", {})
@@ -65,7 +60,7 @@ keymap.set('n', '<leader>s', "<cmd>:Lspsaga show_diagnostics<cr>", {})
 keymap.set("n", "<leader>dn", "<cmd>:Lspsaga diagnostic_jump_next<cr>", {})
 keymap.set("n", "<leader>dp", "<cmd>:Lspsaga diagnostic_jump_prev<cr>", {})
 
-keymap.set("n", "<leader>gc", "<cmd>:GitCoAuthors<cr>", {})
+keymap.set("n", "<leader>gc", "<cmd>:Coauth<cr>", {})
 
 keymap.set("n", "<A-Up>", "<cmd>:tabnew<cr>", {})     -- Alt + Arrow Up, new tab
 keymap.set("n", "<A-Left>", "<cmd>:tabprev<cr>", {})  -- Alt + Arrow Left, tab left
