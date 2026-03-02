e1cba19bdd948bae4513b41dc12449348f8efa64
Kristoffer Dalby <kristoffer@dalby.cc>
2026-03-02 16:21:49 +0000

    flake.nix: migrate plugins from flake inputs to nixpkgs vimPlugins
    Replace 15 flake inputs with pre-built pkgs.vimPlugins equivalents.
    Only vim-plist and github-coauthors.nvim remain as flake inputs (not
    available in nixpkgs). This gives binary cache hits, eliminates lock
    churn, and speeds up nix flake update.
    
    Remove flake-compat (unused), commented-out dead code (DAP plugins,
    templates, old helpers), unused pluginNames/pluginSet bindings, and
    deprecated output names (overlay -> overlays.default, drop redundant
    defaultPackage/defaultApp).

--- flake.nix
+++ flake.nix
@@ -1,168 +1,21 @@
 {
   description = "kradalby's Neovim distribution";
-  inputs =
-    # let
-    #   nvimPlugin = ghurl: let
-    #     pluginName = builtins.elemAt (builtins.split "/" ghurl) 1;
-    #   in {
-    #     "name" = "vim:${pluginName}";
-    #     "value" = {
-    #       url = "github:${ghurl}";
-    #       flake = false;
-    #     };
-    #   };
-    #
-    #   nvimPluginsDef = builtins.listToAttrs (builtins.map nvimPlugin [
-    #     "nvim-dap"
-    #     "nvim-dap-go"
-    #     "nvim-dap-python"
-    #     "rcarriga/nvim-dap-ui"
-    #
-    #     # Libraries/Shared stuff
-    #     "plenary.nvim"
-    #   ]);
-    # in
-    {
-      flake-utils.url = "github:numtide/flake-utils";
 
-      flake-compat = {
-        url = "github:edolstra/flake-compat";
-        flake = false;
-      };
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-
-      # "vim:" = {
-      #   url = "github:";
-      #   flake = false;
-      # };
-
-      "vim:nvim-colorizer.lua" = {
-        url = "github:catgoose/nvim-colorizer.lua";
-        flake = false;
-      };
-
-      "vim:conform.nvim" = {
-        url = "github:stevearc/conform.nvim";
-        flake = false;
-      };
-
-      "vim:nvim-lint" = {
-        url = "github:mfussenegger/nvim-lint";
-        flake = false;
-      };
-
-      "vim:schemastore.nvim" = {
-        url = "github:b0o/schemastore.nvim";
-        flake = false;
-      };
-
-      "vim:nvim-autopairs" = {
-        url = "github:windwp/nvim-autopairs";
-        flake = false;
-      };
-
-      "vim:mini.nvim" = {
-        url = "github:echasnovski/mini.nvim";
-        flake = false;
-      };
-
-      "vim:vim-plist" = {
-        url = "github:darfink/vim-plist";
-        flake = false;
-      };
-
-      "vim:nvim-web-devicons" = {
-        url = "github:nvim-tree/nvim-web-devicons";
-        flake = false;
-      };
-
-      "vim:tokyonight.nvim" = {
-        url = "github:folke/tokyonight.nvim";
-        flake = false;
-      };
-
-      "vim:nvim-neoclip.lua" = {
-        url = "github:AckslD/nvim-neoclip.lua";
-        flake = false;
-      };
-
-      "vim:sqlite.lua" = {
-        url = "github:kkharji/sqlite.lua";
-        flake = false;
-      };
-
-      "vim:gitsigns.nvim" = {
-        url = "github:lewis6991/gitsigns.nvim";
-        flake = false;
-      };
-
-      # "vim:trouble.nvim" = {
-      #   url = "github:folke/trouble.nvim";
-      #   flake = false;
-      # };
-
-      "vim:todo-comments.nvim" = {
-        url = "github:folke/todo-comments.nvim";
-        flake = false;
-      };
-
-      # "vim:nvim-nio" = {
-      #   url = "github:nvim-neotest/nvim-nio";
-      #   flake = false;
-      # };
-      #
-      # "vim:nvim-dap" = {
-      #   url = "github:mfussenegger/nvim-dap";
-      #   flake = false;
-      # };
-      #
-      # "vim:nvim-dap-ui" = {
-      #   url = "github:rcarriga/nvim-dap-ui";
-      #   flake = false;
-      # };
-      #
-      # "vim:nvim-dap-python" = {
-      #   url = "github:mfussenegger/nvim-dap-python";
-      #   flake = false;
-      # };
-      #
-      # "vim:nvim-dap-go" = {
-      #   url = "github:leoluz/nvim-dap-go";
-      #   flake = false;
-      # };
-      #
-      # "vim:nvim-dap-virtual-text" = {
-      #   url = "github:theHamsta/nvim-dap-virtual-text";
-      #   flake = false;
-      # };
-      #
-      # "vim:telescope-dap.nvim" = {
-      #   url = "github:nvim-telescope/telescope-dap.nvim";
-      #   flake = false;
-      # };
-
-      "vim:plenary.nvim" = {
-        url = "github:nvim-lua/plenary.nvim";
-        flake = false;
-      };
-
-      "vim:telescope.nvim" = {
-        url = "github:nvim-telescope/telescope.nvim";
-        flake = false;
-      };
-
-      "vim:github-coauthors.nvim" = {
-        url = "github:cwebster2/github-coauthors.nvim";
-        flake = false;
-      };
-
-      # These require special treatment (ie, compilation), so we can't load them in bulk
-      "telescope-fzf-native.nvim" = {
-        url = "github:nvim-telescope/telescope-fzf-native.nvim";
-        flake = false;
-      };
+    # Plugins not available in nixpkgs
+    "vim:vim-plist" = {
+      url = "github:darfink/vim-plist";
+      flake = false;
     };
+
+    "vim:github-coauthors.nvim" = {
+      url = "github:cwebster2/github-coauthors.nvim";
+      flake = false;
+    };
+  };
 
   outputs = {
     self,
@@ -171,42 +24,23 @@
     ...
   } @ inputs:
     {
-      overlay = final: prev: let
+      overlays.default = final: prev: let
         pkgs = import nixpkgs {
           inherit (prev) system;
-          # overlays = [neovim-nightly-overlay.overlay];
         };
 
-        # Build Vim plugin flake inputs into a list of Nix packages
-        # Helper function to create a basic plugin
-        buildPlugin = n: v: pkgs.vimUtils.buildVimPlugin {
-          name = pkgs.lib.strings.removePrefix "vim:" n;
-          src = v.outPath;
-          namePrefix = "";
-          # Temporarily disable checks while we implement proper dependencies
-          doCheck = false;
-        };
+        # Build plugins not available in nixpkgs from flake inputs
+        buildPlugin = n: v:
+          pkgs.vimUtils.buildVimPlugin {
+            name = pkgs.lib.strings.removePrefix "vim:" n;
+            src = v.outPath;
+            namePrefix = "";
+            doCheck = false;
+          };
 
-        # Build all plugins first
-        basePlugins = pkgs.lib.mapAttrsToList buildPlugin (pkgs.lib.filterAttrs (n: v: pkgs.lib.strings.hasPrefix "vim:" n) inputs);
-
-        # For debugging, let's see what plugin names we actually have
-        pluginNames = map (plugin: plugin.name) basePlugins;
-
-        # Convert to attribute set for easy referencing
-        pluginSet = pkgs.lib.listToAttrs (map (plugin: pkgs.lib.nameValuePair plugin.name plugin) basePlugins);
-
-        # For now, use basic plugins to test if the build works
-        vimPackages = basePlugins;
-
-        telescopeFzfNative = pkgs.vimUtils.buildVimPlugin {
-          name = "telescope-fzf-native.nvim";
-          src = inputs."telescope-fzf-native.nvim".outPath;
-          namePrefix = "";
-          buildPhase = ''
-            make
-          '';
-        };
+        flakePlugins =
+          pkgs.lib.mapAttrsToList buildPlugin
+          (pkgs.lib.filterAttrs (n: _: pkgs.lib.strings.hasPrefix "vim:" n) inputs);
 
         neovim-nix-lua-conf = pkgs.writeText "nix.lua" ''
           vim.g.sqlite_clib_path = "${pkgs.sqlite.out}/lib/${
@@ -216,8 +50,6 @@
           }"
         '';
 
-        # TODO: Only copy *.lua files, maybe with `nix-filter`
-        # Make a derivation containing only Neovim Lua config
         neovim-kradalby-luaconfig = pkgs.stdenv.mkDerivation rec {
           name = "neovim-kradalby-luaconfig";
           src = pkgs.nix-gitignore.gitignoreSource [] ./.;
@@ -230,27 +62,50 @@
           '';
         };
       in {
-        # Wrap Neovim with custom plugins and config
         neovim-kradalby = pkgs.neovim.override {
           viAlias = true;
           vimAlias = true;
           withNodeJs = false;
 
           configure = {
-            packages.kradalby = with pkgs.vimPlugins; {
+            packages.kradalby = {
               start =
-                [
-                  # Filter out tlaplus grammar due to nixpkgs bug with scoped npm package names
-                  # The @tlaplus/tree-sitter-tlaplus package causes grammarToPlugin to fail
+                (with pkgs.vimPlugins; [
+                  # Treesitter (all grammars, minus tlaplus due to nixpkgs bug)
                   # See: https://github.com/NixOS/nixpkgs/issues/341442
                   (nvim-treesitter.withPlugins (_:
                     builtins.filter (g: !(pkgs.lib.hasInfix "tlaplus" (pkgs.lib.getName g)))
-                      pkgs.tree-sitter.allGrammars
-                  ))
-                  telescopeFzfNative
+                    pkgs.tree-sitter.allGrammars))
+
+                  # Completion
                   blink-cmp
-                ]
-                ++ vimPackages;
+
+                  # LSP
+                  SchemaStore-nvim
+
+                  # Formatting & Linting
+                  conform-nvim
+                  nvim-lint
+
+                  # Telescope
+                  telescope-nvim
+                  telescope-fzf-native-nvim
+                  plenary-nvim
+
+                  # UI
+                  tokyonight-nvim
+                  nvim-web-devicons
+                  mini-nvim
+
+                  # Git
+                  gitsigns-nvim
+
+                  # Editor
+                  todo-comments-nvim
+                  nvim-neoclip-lua
+                  sqlite-lua
+                ])
+                ++ flakePlugins;
             };
 
             customRC = ''
@@ -265,20 +120,16 @@
       system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [self.overlay];
+          overlays = [self.overlays.default];
         };
-      in rec {
-        packages = with pkgs; {
-          inherit neovim-kradalby;
-
-          default = neovim-kradalby;
+      in {
+        packages = {
+          inherit (pkgs) neovim-kradalby;
+          default = pkgs.neovim-kradalby;
         };
 
-        defaultPackage = packages.neovim-kradalby;
-        apps.neovim-kradalby = flake-utils.lib.mkApp {drv = packages.neovim-kradalby;};
-        defaultApp = apps.neovim-kradalby;
-
-        overlays.default = self.overlay;
+        apps.neovim-kradalby = flake-utils.lib.mkApp {drv = pkgs.neovim-kradalby;};
+        apps.default = flake-utils.lib.mkApp {drv = pkgs.neovim-kradalby;};
       }
     );
 }
