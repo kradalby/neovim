a192bd98b66cf9623fc8f20d7483bf682149183c
Kristoffer Dalby <kristoffer@dalby.cc>
2026-04-16 08:27:23 +0200

    flake: switch nixpkgs to unstable and update inputs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
 
     # Plugins not available in nixpkgs
     "vim:vim-plist" = {
